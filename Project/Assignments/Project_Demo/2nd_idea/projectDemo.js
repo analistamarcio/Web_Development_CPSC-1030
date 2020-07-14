4158948746111c895af41be09c865e30c2dead63
Marcio Ferreira <analistamarcio@hotmail.com>
2020-07-14 12:25:12 -0700

    Midter2 and folders improvement

--- Project/Assignments/Project_Demo/2nd_idea/projectDemo.js
+++ Project/Assignments/Project_Demo/2nd_idea/projectDemo.js
@@ -1,5 +1,33 @@
-document.getElementsByTagName("main");
+let control = document.getElementById("wrapper");
 
-function showAlert() {
-  return alert("Hi!");
+control.addEventListener("wheel", scrolling);
+let posX = 0;
+
+
+function scrolling() {
+  if (control.scrollTop > 0) {
+    posX++;
+    control.scrollTop = 0;
+  } else {
+    posX--;
+  }
+
+  console.log("posX = " + posX);
 }
+
+// control.addEventListener('scroll', function (e) {
+//   // get the scrollDemo
+//   let div = document.getElementById('scrollDemo');
+//   // get the target
+//   let target = e.target;
+//   // handle each button's click
+//   switch (target.id) {
+//     case 'btnScrollLeft':
+//       div.scrollLeft += 20;
+//       break;
+
+//     case 'btnScrollTop':
+//       div.scrollTop += 20;
+//       break;
+//   }
+// });
